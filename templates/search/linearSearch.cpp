0a69d9374d21ab30d76c18ee43e52dc6d864f121
Neel <neel.pendharkar@gmail.com>
2026-01-09 08:18:52 +0530

    Add linear search #2

---
+++ templates/search/linearSearch.cpp
@@ -0,0 +1,13 @@
+
+void solve() {
+    int n, s;
+    cin>>n>>s;
+    
+    int a[n];
+    fo(i,n) cin>>a[i];
+
+    int id = -1;
+    fo(i,n) if(a[i]==s) { id = i; break; }
+
+    cout<<id<<'\n';
+}
